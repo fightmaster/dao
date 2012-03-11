6f8175c8c061971db6a5d88194b3f0493babb1b7
Dmitry Petrov <old.fightmaster@gmail.com>
2012-03-11 10:16:53 +0400

    Added License information to header

--- tests/Fightmaster/Tests/Model/Manager/DoctrineManagerTest.php
+++ tests/Fightmaster/Tests/Model/Manager/DoctrineManagerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Fightmaster/dao library.
+ *
+ * (c) Dmitry Petrov aka fightmaster <old.fightmaster@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Fightmaster\Tests\Model\Manager;
 
 
